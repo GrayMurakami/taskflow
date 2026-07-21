72ed1e01474e012f33249ab9360821299f5d7f5e
Gray <digrayman8@gmail.com>
2026-07-21 13:30:27 +0900

    feat: update task and delete task functional

--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -6,7 +6,7 @@ import { useTasks } from '@/entities/task/useTasks'
 import TaskCreateForm from '@/features/task-create/TaskCreateForm.vue'
 
 const { logout, user } = useAuth();
-const { tasks, isLoading, error, fetchTasks } = useTasks();
+const { tasks, isLoading, error, fetchTasks, updateTask, deleteTask } = useTasks();
 const router = useRouter();
 
 onMounted(() => {
@@ -16,6 +16,23 @@ onMounted(() => {
 async function handleLogout() {
   await logout();
   router.push('/login');
+}
+
+async function toggleStatus(id: string, currentStatus: 'active' | 'done') {
+  const newStatus = currentStatus === 'active' ? 'done' : 'active';
+  const success = await updateTask(id, { status: newStatus });
+
+  if (!success) {
+    console.error('Failed to update the task status!');
+  }
+}
+
+async function handleDelete(id: string) {
+  const success = await deleteTask(id);
+
+  if (!success) {
+    console.error('Failed to delete the task!')
+  }
 }
 </script>
 
@@ -29,13 +46,31 @@ async function handleLogout() {
 
     <TaskCreateForm />
 
+    <p v-if="error" class="error">Tasks Error: {{ error }}</p>
+
     <p v-if="isLoading">Loading Tasks ・・・</p>
-    <p v-else-if="error">Tasks Error: {{ error }}</p>
-    <p v-else-if="tasks.length === 0">There are no tasks yet.</p>
+    <p v-else-if="tasks.length === 0 && !error">There are no tasks yet.</p>
 
     <ul v-else>
       <li v-for="task in tasks" :key="task.id">
-        {{ task.title }} - {{ task.priority }} - {{ task.status }}
+        <input
+          type="checkbox"
+          :checked="task.status === 'done'"
+          @change="toggleStatus(task.id, task.status)" 
+        />
+        <span :class="{ done: task.status === 'done' }">
+          {{ task.title }} - {{ task.priority }}
+        </span>
+        <button @click="handleDelete(task.id)">
+          Delete
+        </button>
       </li>
     </ul>
 </template>
+
+<style scoped>
+.done {
+  text-decoration: line-through;
+  opacity: 0.6;
+}
+</style>
