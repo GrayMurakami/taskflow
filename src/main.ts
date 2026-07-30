e9c4ec06b271182693e9efafe212d4296e1f8069
Gray <digrayman8@gmail.com>
2026-07-30 23:21:34 +0900

    fix: main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import './style.css'
-import App from './App.vue'
-import router from './app/router'
 import { useAuth } from '@/entities/user/useAuth'
+import App from '@/App.vue'
+import router from '@/app/router'
+import './style.css'
 
 async function bootstrap() {
   const app = createApp(App);
